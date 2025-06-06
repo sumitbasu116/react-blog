44c501d482fcc957e701e486fbca2551638239f3
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-06 23:09:04 +0530

    as a variable prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import User from "./User";
 
 function App(){
-
+let userName="Sumit Basu";
+let age =32;
 return (
   <div>
     <h1>React JS Props</h1>
-    < User name="Sumit Basu" age='32'/>
+    < User name={userName} age={age}/>
   </div>
 )
 }
