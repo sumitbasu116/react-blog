a4b5ee9eee9ff919ca3ab58cb2676702a6beccf2
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-08 22:16:07 +0530

    Get Input Field Value part3

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ const[val,setVal] = useState('Sumit Basu');
 return (
   <div>
     <h1>React JS Get Input Field Value</h1>
-    <input type="text" onChange={(event)=>setVal(event.target.value)}/>
+    <input type="text" value={val} onChange={(event)=>setVal(event.target.value)}/>
     <br />
     <br />
     <h2>{val}</h2>
-    <button onClick={()=>setVal("")}>Clear Text</button>
+    <button onClick={()=>setVal("")}>Clear</button>
   </div>
 )
 }
