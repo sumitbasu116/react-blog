7835c36340509635533d39653219bcd8ce546df4
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-11 23:07:22 +0530

    Handle Checkbox part2

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,25 +3,25 @@ function Skills(){
 const[skills,setSkills] = useState(['']);
 
 function handleSkills(event){
-    console.log(event.target.value);
-
+    console.log(event.target.value,event.target.checked);
+    setSkills(event.target.value);
 }
     return(
         <div>
             <h3>Select your Skills</h3>
-            <input type="checkbox" onChange={(event)=>{handleSkills(event)}} id='php' value='PHP'/>
+            <input type="checkbox" onChange={handleSkills} id='php' value='PHP'/>
             <label htmlFor="php">PHP</label>
             <br />
             <br />
-            <input type="checkbox" onChange={(event)=>{handleSkills(event)}} id='js' value='JS'/>
+            <input type="checkbox" onChange={handleSkills} id='js' value='JS'/>
             <label htmlFor="js">JS</label>
             <br />
             <br />
-            <input type="checkbox" onChange={(event)=>{handleSkills(event)}} id='node' value='Node'/>
+            <input type="checkbox" onChange={handleSkills} id='node' value='Node'/>
             <label htmlFor="node">NODE</label>
             <br />
             <br />
-            <input type="checkbox" onChange={(event)=>{handleSkills(event)}} id='java' value='JAVA'/>
+            <input type="checkbox" onChange={handleSkills} id='java' value='JAVA'/>
             <label htmlFor="java">JAVA</label>
 
         <h2>{skills.toString()}</h2>
