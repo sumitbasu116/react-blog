6b2e6bce89a3f0ddc2e88a775feb5adeb06d920d
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-12 21:00:29 +0530

    Radio Button part2

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ return (
   <div>
     <h1>Handle Radio Button and Dropdown</h1>
     <h4>Select Gender:</h4> 
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' />
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' value={'male'}/>
     <label htmlFor="male">Male</label>
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female'/>
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female' value={'female'}/>
     <label htmlFor="female">Female</label>
     <h3>Selected Gender:{gender}</h3>
   </div>
