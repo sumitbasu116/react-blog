e4da8437b0f678dd18d63d24afcb160ce7fa938d
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-12 21:39:24 +0530

    Each child in a list should have a unique key prop-->error fix

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           {
             userData.map(
               (user) => (
-                <tr>
+                <tr key={user.id}>
                   <td>{user.id}</td>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
