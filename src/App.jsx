18c2e26695ee009140d71e73705fabeae1d079bc
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-12 20:57:06 +0530

    Radio Button part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Skills from "./Skills";
 function App(){
-  
+  const[gender,setGender] = useState('male');
 return (
   <div>
-    <h1>Handle Checkbox</h1>
-    <Skills />
+    <h1>Handle Radio Button and Dropdown</h1>
+    <h4>Select Gender:</h4> 
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' />
+    <label htmlFor="male">Male</label>
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female'/>
+    <label htmlFor="female">Female</label>
+    <h3>Selected Gender:{gender}</h3>
   </div>
 )
 }
