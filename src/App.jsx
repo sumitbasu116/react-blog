b9b0ab047d71d1a801b3ff4a00596a8f9ae9e8b3
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-12 21:03:29 +0530

    default radio button

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ return (
   <div>
     <h1>Handle Radio Button and Dropdown</h1>
     <h4>Select Gender:</h4> 
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' value={'male'}/>
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' value={'male'} 
+    checked={gender=='male'}/>
     <label htmlFor="male">Male</label>
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female' value={'female'}/>
+    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female' value={'female'}
+    checked={gender=='female'}/>
     <label htmlFor="female">Female</label>
     <h3>Selected Gender:{gender}</h3>
   </div>
