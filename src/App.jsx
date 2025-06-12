fa65dce92d617c2809949e52091600f6f0ddcd5a
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-12 21:33:14 +0530

    Loop in JSX part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import { useState } from "react";
-import Skills from "./Skills";
+
 function App(){
-  const[gender,setGender] = useState('male');
-  const[city,setCity] = useState('delhi');
+  const userData=[
+    {
+      name:'Sumit',
+      age:32,
+      email:'sumit@test.com',
+      id:1
+    },
+    {
+      name:'Amit',
+      age:42,
+      email:'amit@test.com',
+      id:2
+    },
+    {
+      name:'Arit',
+      age:22,
+      email:'arit@test.com',
+      id:3
+    },
+    {
+      name:'Sunil',
+      age:52,
+      email:'sunil@test.com',
+      id:4
+    }
+  ];
 return (
   <div>
-    <h1>Handle Radio Button and Dropdown</h1>
-    <h4>Select Gender:</h4> 
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='male' value={'male'} 
-    checked={gender=='male'}/>
-    <label htmlFor="male">Male</label>
-    <input type="radio" name='gender' onChange={(event)=>{setGender(event.target.value)}} id='female' value={'female'}
-    checked={gender=='female'}/>
-    <label htmlFor="female">Female</label>
-    <h3>Selected Gender:{gender}</h3>
-    <h4>Select City:</h4>
-    <select onChange={(event)=>setCity(event.target.value)}>
-      <option value="delhi">Delhi</option>
-      <option value="kolkata">Kolkata</option>
-      <option value="mumbai">Mumbai</option>
-    </select>
-     <h3>Selected City:{city}</h3>
+    <h1>Loop in JSX with Map Function</h1>
+    <table border='1'>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>NAME</td>
+          <td>EMAIL</td>
+          <td>AGE</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{userData[0].id}</td>
+          <td>{userData[0].name}</td>
+          <td>{userData[0].email}</td>
+          <td>{userData[0].age}</td>
+        </tr>
+        <tr>
+          <td>{userData[1].id}</td>
+          <td>{userData[1].name}</td>
+          <td>{userData[1].email}</td>
+          <td>{userData[1].age}</td>
+        </tr>
+        <tr>
+          <td>{userData[2].id}</td>
+          <td>{userData[2].name}</td>
+          <td>{userData[2].email}</td>
+          <td>{userData[2].age}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 )
 }
