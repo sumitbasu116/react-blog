cfc5c2e367f266ab73a4849e1bfcf7d32f5de3b0
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-13 22:48:26 +0530

    toggle to hide or unmounting a component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ function App() {
 
   const[counter,setCounter]=useState(0);
   const[data,setData]=useState(0);
+  const[display,setDisplay]=useState(true);
 
   return (
     <div>
-
-    <Counter data={data} counter={counter}/>
+    
+    {
+      display?<Counter data={data} counter={counter}/>:null
+    }
     <button onClick={()=>setCounter(counter+1)}>Counter</button>
     <button onClick={()=>setData(data+1)}>Data</button>
+    <button onClick={()=>setDisplay(!display)}>Toggle</button>
     </div>
   )
 }
