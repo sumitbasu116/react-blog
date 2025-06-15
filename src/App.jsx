a9aa75d36400d062d8987f64929255c1454523bd
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-15 09:31:53 +0530

    display flex and flex wrap

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,66 @@ function App() {
 
   return (
     <>
-      <div style={styling}>
-        <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
-        <img style={{
-          width: '100px',
-          borderRadius: '10px',
-          margin: '5px'
-        }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-        <div style={{ padding: '5px' }}>
-          <h4>Software Developer</h4>
-          <p>Sumit Basu</p>
+      <div style={{display:'flex',flexWrap:'wrap'}}>
+        <div style={styling}>
+          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+          <img style={{
+            width: '100px',
+            borderRadius: '10px',
+            margin: '5px'
+          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div style={{ padding: '5px' }}>
+            <h4>Software Developer</h4>
+            <p>Sumit Basu</p>
+          </div>
         </div>
-      </div>
-      <div style={styling}>
-        <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
-        <img style={{
-          width: '100px',
-          borderRadius: '10px',
-          margin: '5px'
-        }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-        <div style={{ padding: '5px' }}>
-          <h4>Software Developer</h4>
-          <p>Sumit Basu</p>
+        <div style={styling}>
+          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+          <img style={{
+            width: '100px',
+            borderRadius: '10px',
+            margin: '5px'
+          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div style={{ padding: '5px' }}>
+            <h4>Software Developer</h4>
+            <p>Sumit Basu</p>
+          </div>
+        </div>
+        <div style={styling}>
+          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+          <img style={{
+            width: '100px',
+            borderRadius: '10px',
+            margin: '5px'
+          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div style={{ padding: '5px' }}>
+            <h4>Software Developer</h4>
+            <p>Sumit Basu</p>
+          </div>
+        </div>
+        <div style={styling}>
+          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+          <img style={{
+            width: '100px',
+            borderRadius: '10px',
+            margin: '5px'
+          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div style={{ padding: '5px' }}>
+            <h4>Software Developer</h4>
+            <p>Sumit Basu</p>
+          </div>
+        </div>
+        <div style={styling}>
+          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+          <img style={{
+            width: '100px',
+            borderRadius: '10px',
+            margin: '5px'
+          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div style={{ padding: '5px' }}>
+            <h4>Software Developer</h4>
+            <p>Sumit Basu</p>
+          </div>
         </div>
       </div>
     </>
