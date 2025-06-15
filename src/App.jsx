07955b235df0a07e6b9954d868c17c0a9484d3be
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-15 10:43:00 +0530

    dynamic styling in div and text color separately

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,59 @@
-
-
-
+import { useState } from "react";
 function App() {
 
-  const styling = {
-    border: '1px solid #483131',
-    backgroundColor: 'cadetblue',
-    borderRadius: '10px',
-    width: '300px',
-    margin: '2px', boxShadow: '2px 5px 5px #766d6d',
-    paddingLeft: '50px'
-  };
+  const [cardstyle, setCardstyle] = useState(
+    {
+      border: '1px solid #483131',
+      borderRadius: '10px',
+      width: '300px',
+      margin: '2px', boxShadow: '2px 5px 5px #766d6d',
+      paddingLeft: '50px'
+    }
+  )
+
+  const updateTheme = (bgColor, textColor) => {
+    setCardstyle(
+      {
+        ...cardstyle, backgroundColor: bgColor
+      }
+    ),
+    setTextColor(textColor)
+  }
+
+  const [textColor, setTextColor] = useState('black');
+
 
   return (
     <>
-      <div style={{display:'flex',flexWrap:'wrap'}}>
-        <div style={styling}>
-          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+      <button onClick={() => updateTheme('cadetblue', '#3F51B5')}>Custom</button>
+      <button onClick={() => updateTheme('white', 'black')}>Default</button>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+
+        <div style={cardstyle}>
+          <h2>Dynamic and Conditional Inline Style</h2>
           <img style={{
             width: '100px',
             borderRadius: '10px',
             margin: '5px'
           }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px' }}>
+          <div style={{ padding: '5px',color:textColor}}>
             <h4>Software Developer</h4>
             <p>Sumit Basu</p>
           </div>
         </div>
-        <div style={styling}>
-          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+        <div style={cardstyle}>
+          <h2>Inline Style in React</h2>
           <img style={{
             width: '100px',
             borderRadius: '10px',
             margin: '5px'
           }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px' }}>
+          <div style={{ padding: '5px',color:textColor}}>
             <h4>Software Developer</h4>
             <p>Sumit Basu</p>
           </div>
         </div>
-        <div style={styling}>
-          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
-          <img style={{
-            width: '100px',
-            borderRadius: '10px',
-            margin: '5px'
-          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px' }}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        <div style={styling}>
-          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
-          <img style={{
-            width: '100px',
-            borderRadius: '10px',
-            margin: '5px'
-          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px' }}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        <div style={styling}>
-          <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
-          <img style={{
-            width: '100px',
-            borderRadius: '10px',
-            margin: '5px'
-          }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px' }}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
+        
       </div>
     </>
   )
