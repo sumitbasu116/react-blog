c480f2568ee69aeecb68f5d3cbd49178aee53418
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-15 09:24:29 +0530

    const styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@
 
 function App() {  
 
-
-
-  return (
-    <div style={{border: '1px solid #483131',
+const styling={border: '1px solid #483131',
     backgroundColor: 'cadetblue',
     borderRadius: '10px',
-    width: '500px',
+    width: '300px',
     margin: '2px',boxShadow: '2px 5px 5px #766d6d',
-    paddingLeft: '50px'}}>
+    paddingLeft: '50px'};
+
+  return (
+    <div style={styling}>
       <h2 style={{color:'#3F51B5'}}>Inline Style in React</h2>
-    <img style={{width: '200px',
+    <img style={{width: '100px',
     borderRadius: '10px',
     margin: '5px'}} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+    <div style={{padding:'5px'}}>
     <h4>Software Developer</h4>
     <p>Sumit Basu</p>
+    </div>
     </div>
   )
 }
