d08610b7f6e442b6cc2a366381cfcae5a848f3a2
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-15 09:25:57 +0530

    react fragment to group child elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 
 
 
-function App() {  
+function App() {
 
-const styling={border: '1px solid #483131',
+  const styling = {
+    border: '1px solid #483131',
     backgroundColor: 'cadetblue',
     borderRadius: '10px',
     width: '300px',
-    margin: '2px',boxShadow: '2px 5px 5px #766d6d',
-    paddingLeft: '50px'};
+    margin: '2px', boxShadow: '2px 5px 5px #766d6d',
+    paddingLeft: '50px'
+  };
 
   return (
-    <div style={styling}>
-      <h2 style={{color:'#3F51B5'}}>Inline Style in React</h2>
-    <img style={{width: '100px',
-    borderRadius: '10px',
-    margin: '5px'}} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-    <div style={{padding:'5px'}}>
-    <h4>Software Developer</h4>
-    <p>Sumit Basu</p>
-    </div>
-    </div>
+    <>
+      <div style={styling}>
+        <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+        <img style={{
+          width: '100px',
+          borderRadius: '10px',
+          margin: '5px'
+        }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+        <div style={{ padding: '5px' }}>
+          <h4>Software Developer</h4>
+          <p>Sumit Basu</p>
+        </div>
+      </div>
+      <div style={styling}>
+        <h2 style={{ color: '#3F51B5' }}>Inline Style in React</h2>
+        <img style={{
+          width: '100px',
+          borderRadius: '10px',
+          margin: '5px'
+        }} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+        <div style={{ padding: '5px' }}>
+          <h4>Software Developer</h4>
+          <p>Sumit Basu</p>
+        </div>
+      </div>
+    </>
   )
 }
 export default App;
