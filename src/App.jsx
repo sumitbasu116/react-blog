31e6e0689c92fd8811fe94fbf6f801777043979f
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-16 22:42:59 +0530

    styled-components example

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import './css/styling.css'
-import UserProfile from './UserProfile';
+import styled from "styled-components";
 function App() {
 
+  const Heading=styled.h2(
+    {
+      color:'red',
+      border:'1px solid green',
+      borderRadius:'5px',
+      margin:'20px',
+      padding:'10px'
+    }
+  )
+  const ButtonSt=styled.button`
+  color:red;
+  width:100px;
+  height:30px;
+  margin: 20px
+  `
 
   return (
-    <div className='container'>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
-      <UserProfile/>
+    <div>
+      <h1>Styled components</h1>
+      <Heading>Hello Style Component 1</Heading>
+      <Heading>Hello Style Component 2</Heading>
+      <Heading>Hello Style Component 3</Heading>
+      <Heading>Hello Style Component 4</Heading>
+      <Heading>Hello Style Component 5</Heading>
+      <Heading>Hello Style Component 6</Heading>
+      <ButtonSt>Login</ButtonSt>
+      <ButtonSt>Register</ButtonSt>
+      <ButtonSt>Back</ButtonSt>
     </div>
   )
 }
