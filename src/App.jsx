043e742763309ec5678a20695c376cf1cca8a622
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-16 09:33:38 +0530

    advance styling flex part1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1 className='heading'>External Styling</h1>
+      <div className='container'>
       <div className='user-card'>
         <div>
           <img className='img-style' src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
@@ -13,6 +14,25 @@ function App() {
           <h4>Software Developer</h4>
           <p>Sumit Basu</p>
         </div>
+      </div>
+      <div className='user-card'>
+        <div>
+          <img className='img-style' src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+        </div>
+        <div className='text-wrap'>
+          <h4>Software Developer</h4>
+          <p>Sumit Basu</p>
+        </div>
+      </div>
+      <div className='user-card'>
+        <div>
+          <img className='img-style' src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+        </div>
+        <div className='text-wrap'>
+          <h4>Software Developer</h4>
+          <p>Sumit Basu</p>
+        </div>
+      </div>
       </div>
     </>
   )
