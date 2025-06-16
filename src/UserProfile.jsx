b63cd7572f2278d46ee84cdc9a2eb2b1597df72f
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-16 11:07:29 +0530

    css module style part2

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -2,12 +2,14 @@ import style from './css/UserProfile.module.css'
 function UserProfile(){
     return(
         <div>
+            <div className={style.card}>
             <div>
-                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+                <img className={style.imgStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
             </div>
-            <div>
+            <div className={style.textWrap}>
                 <h4 className={style.heading}>Sumit Basu</h4>
                 <p>Software Developer</p>
+            </div>
             </div>
         </div>
     )
