cd4d7e5f974ca64ea2eed6874055603b9874b125
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-16 23:10:30 +0530

    react-bootstrap part2 using alert and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import {Button} from 'react-bootstrap';
-import {Dropdown} from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert';
+import {NavDropdown,Navbar,Nav,Container} from 'react-bootstrap';
+
 function App() {
 
   return (
     <div>
       <h2>Add Bootstrap with React js</h2>
-      <Button>Ok</Button>
-      <Button variant='danger'>Ok</Button>
-      <Button variant='success'>Ok</Button>
-      <Button variant="outline-success">Success</Button>
+      <Alert variant='danger'>This is a danger alert—check it out!</Alert>
+      <Alert variant='success'>This is a success alert—check it out!</Alert>
       <br />
       <br />
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </div>
   )
 }
