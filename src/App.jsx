b0c94889522df1c0d573cdf8b16d33ca1f6b440d
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-16 09:31:39 +0530

    external basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,18 @@
-import { useState } from "react";
+import './css/styling.css'
 function App() {
 
-  const [cardstyle, setCardstyle] = useState(
-    {
-      border: '1px solid #483131',
-      borderRadius: '10px',
-      width: '300px',
-      margin: '2px', boxShadow: '2px 5px 5px #766d6d',
-      paddingLeft: '50px'
-    }
-  )
-  const imageStyle={width: '100px',
-            borderRadius: '10px',
-            margin: '5px'};
-  const updateTheme = (bgColor, textColor) => {
-    setCardstyle(
-      {
-        ...cardstyle, backgroundColor: bgColor
-      }
-    ),
-    setTextColor(textColor)
-  }
-
-  const [textColor, setTextColor] = useState('black');
-
-  const[grid,toggleGrid] = useState(true);
 
   return (
     <>
-      <button onClick={() => updateTheme('cadetblue', '#3F51B5')}>Custom</button>
-      <button onClick={() => updateTheme('white', 'black')}>Default</button>
-      <button onClick={() => toggleGrid(!grid)}>Toogle Grid</button>
-      <div style={{ display: grid?'flex':'block', flexWrap: 'wrap' }}>
-
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Dynamic and Conditional Inline Style</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
+      <h1 className='heading'>External Styling</h1>
+      <div className='user-card'>
+        <div>
+          <img className='img-style' src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
         </div>
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Dynamic and Conditional Inline Style</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
+        <div className='text-wrap'>
+          <h4>Software Developer</h4>
+          <p>Sumit Basu</p>
         </div>
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Dynamic and Conditional Inline Style</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Dynamic and Conditional Inline Style</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Dynamic and Conditional Inline Style</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        <div style={{...cardstyle,color:textColor}}>
-          <h2>Inline Style in React</h2>
-          <img style={imageStyle} src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-          <div style={{ padding: '5px'}}>
-            <h4>Software Developer</h4>
-            <p>Sumit Basu</p>
-          </div>
-        </div>
-        
       </div>
     </>
   )
