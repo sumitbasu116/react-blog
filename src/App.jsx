42f93ec87ea36f84e37c1d798734d653cff23023
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-17 09:55:15 +0530

    hide and show using useRef - not recommended

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,18 @@ function App() {
     inputRef.current.value=123;
   }
 
+  const toggleHandler=()=>{
+    if(inputRef.current.style.display!='none'){
+      inputRef.current.style.display='none';
+    }else{
+      inputRef.current.style.display='inline';
+    }
+  }
+
   return (
     <div>
       <h1>useRef Hook</h1>
+      <button onClick={toggleHandler}>Toggle</button>
       <input ref={inputRef} type="text" placeholder='Enter user name'/>
       <button onClick={inputHandler}>Focus on Input Field</button>
     </div>
