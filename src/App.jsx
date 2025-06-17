c839360d3cc1ca18abd4b943c1371c321dbbab74
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-17 09:42:28 +0530

    useRef hook part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import Alert from 'react-bootstrap/Alert';
-import { NavDropdown, Navbar, Nav, Container } from 'react-bootstrap';
+import { useRef } from "react";
 
 function App() {
 
+  const inputRef = useRef(null);
+
+  const inputHandler=()=>{
+    console.log(inputRef);
+    
+  }
+
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <h2>Add Bootstrap with React js</h2>
-      <Alert variant='danger'>This is a danger alert—check it out!</Alert>
-      <Alert variant='success'>This is a success alert—check it out!</Alert>
-      <br />
-      <br />
-
+      <h1>useRef Hook</h1>
+      <input ref={inputRef} type="text" placeholder='Enter user name'/>
+      <button onClick={inputHandler}>Focus on Input Field</button>
     </div>
   )
 }
