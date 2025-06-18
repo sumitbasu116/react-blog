4600cd5f0e72f6fa57a9c998310b4889048dad04
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-18 22:41:25 +0530

    uncontrolled component using dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,27 @@
-import { useRef } from "react";
+
 
 function App() {
 
-  const inputRef = useRef(null);
-
-  const inputHandler=()=>{
-    console.log(inputRef);
-    inputRef.current.focus();
-    inputRef.current.style.color='red';
-    inputRef.current.placeholder='Enter Password';
-    inputRef.current.value=123;
+  const handleForm=(event)=>{
+    event.preventDefault();
+    let user=document.querySelector('#user').value;
+    console.log(user);
+    let password=document.querySelector('#password').value;
+    console.log(password);
   }
-
-  const toggleHandler=()=>{
-    if(inputRef.current.style.display!='none'){
-      inputRef.current.style.display='none';
-    }else{
-      inputRef.current.style.display='inline';
-    }
-  }
-const h1Ref = useRef(null);
-const h1RefHandler=()=>{
-  h1Ref.current.style.color='green';
-}
+  
   return (
     <div>
-      <h1>useRef Hook</h1>
-      <button onClick={toggleHandler}>Toggle</button>
-      <input ref={inputRef} type="text" placeholder='Enter user name'/>
-      <button onClick={inputHandler}>Focus on Input Field</button>
-      <h1 ref={h1Ref}>Hello, Sumit Basu</h1>
-      <button onClick={h1RefHandler}>Hello</button>
+      <h1>uncontrolled component</h1>
+      <form action="" onSubmit={handleForm}>
+        <input type="text" id="user" placeholder="Enter your name"/>
+        <br />
+        <br />
+        <input type="text" id="password" placeholder="Enter password"/>
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
