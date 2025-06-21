7c1657a8b4d04dd477dd85e090644daccebb717f
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-22 00:23:55 +0530

    useFormStatus Hook part2-adding promise in code

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ function App() {
 
   const handleForm=()=>{
     console.log("Hello");
-    
+    new Promise(res=>setTimeout(res,2000))
   }
   
   return (
     <div>
       <form action={handleForm}>
         <input type="text" placeholder="Enter Your Name"/>
+        <br />
+        <br />
         <input type="text" placeholder="Enter Password"/>
+        <br />
+        <br />
         <button>Submit</button>
       </form>
     </div>
