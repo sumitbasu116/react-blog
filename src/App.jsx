8d0b29ef9a6bf520b62d8618ab0eded1747c7929
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-23 11:03:03 +0530

    useTransition hook part1-basic structure before actual logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
+import { useState } from "react";
 
-import { useFormStatus } from "react-dom";
 
 function App() {
 
-  const handleForm=async ()=>{
-    await new Promise(res=>setTimeout(res,2000));
-    console.log("Submit");
-  }
-  
-  const CustomerForm=()=>{
-    const {pending}=useFormStatus(null);
-    console.log(pending);
-    
-    return(
-      <div>
-        <input type="text" placeholder="Enter Your Name"/>
-        <br />
-        <br />
-        <input type="text" placeholder="Enter Password"/>
-        <br />
-        <br />
-        <button disabled={pending}>{pending?'Submitting...':'Submit'}</button>
-      </div>
-    )
+  const [pending,setPending] = useState(null);
+
+  const handleButton=()=>{
+    setPending(true);
+    //big logic execution which takes 2sec
+
+    setPending(false);
   }
 
   return (
     <div>
-      <form action={handleForm}>
-        <CustomerForm/>
-      </form>
+      <h1>useTransition Hook</h1>
+      <button disabled={pending} onClick={handleButton}>Click</button>
     </div>
   )
 }
