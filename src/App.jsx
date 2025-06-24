3056d5a229f44ea98cbe0fc0e2c83fcc8f0605d8
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-24 23:26:00 +0530

    useTransition hook part4-adding loader gif

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,10 @@ function App() {
   return (
     <div>
       <h1>useTransition Hook</h1>
+      {
+        pending?
+        <img style={{width:'100px'}} src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="" />:null
+      }
       <button disabled={pending} onClick={handleButton}>Click</button>
     </div>
   )
