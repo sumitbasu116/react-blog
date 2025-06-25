20b29f0b2cf1af9c259e7a2df05819f8c66f4991
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-25 09:56:29 +0530

    Derived State part 3-display the last added user

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ const handleAddUsers=()=>{
 }
 
 const totalUsers=users.length;
+const last = users[users.length-1];
 
   return (
     <div>
       <h2>Total User:{totalUsers}</h2>
-      <h2>Last User:{}</h2>
+      <h2>Last User:{last}</h2>
       <h2>Total Unique User:{}</h2>
       <input type="text" onChange={(event)=>setUser(event.target.value)} placeholder="Add New User"/>
       <button onClick={handleAddUsers}>Add User</button>
