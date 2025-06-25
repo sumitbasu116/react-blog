2b182a6ed0227c4b94367d55e3c185fe84b357e0
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-25 09:54:12 +0530

    Derived State part 2-display total number of users

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ const handleAddUsers=()=>{
   serUsers([...users,user]);
 }
 
+const totalUsers=users.length;
+
   return (
     <div>
-      <h2>Total User:{}</h2>
+      <h2>Total User:{totalUsers}</h2>
       <h2>Last User:{}</h2>
       <h2>Total Unique User:{}</h2>
       <input type="text" onChange={(event)=>setUser(event.target.value)} placeholder="Add New User"/>
