1d3554523c0aeb45f9d500c9a27a242b95aedebb
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-29 11:03:04 +0530

    Updating Array in State part3-Updating Array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,23 @@ function App() {
       'Sam','Ram','Jadu','Madhu'
     ]
   );
+  const [dataDetail,setDataDetails]=useState(
+    [
+      {Name:'Sam',Age:29},
+      {Name:'Ram',Age:23},
+      {Name:'Jadu',Age:24},
+      {Name:'Madhu',Age:22}
+    ]
+  );
   function handleLastNameUpdate(name){
     data[data.length-1] = name;
     setData([...data]);
+  }
+  function handleLastElementAgeUpdate(value){
+    let tempData=dataDetail[data.length-1];
+    tempData.Age=value;
+    dataDetail[data.length-1]=tempData;
+    setDataDetails([...dataDetail]);
   }
   return (
     <div>
@@ -19,6 +33,15 @@ function App() {
         data.map((item,index)=>(
           <h3 key={index}>{item}</h3>
         ))
+      }
+      <hr />
+      <input type="number" placeholder="Update Last Element Age" onChange={(e)=>handleLastElementAgeUpdate(e.target.value)}/>
+      {
+        dataDetail.map(
+          (item,index)=>(
+            <h3 key={index}>{item.Name},{item.Age}</h3>
+          )
+        )
       }
     </div>
   )
