46406fd75c5155df257f258672f305e7fe037c65
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-29 11:56:42 +0530

    useActionState Hook part6-keep the validation message outside the form and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import { useActionState } from "react";
 function App() {
 
-   async function handleFormSubmit(previousData,formData){
-    
-    let name =formData.get('name');
+  async function handleFormSubmit(previousData, formData) {
+
+    let name = formData.get('name');
     let password = formData.get('password');
 
-    await new Promise(res=>setTimeout(res,2000));
+    await new Promise(res => setTimeout(res, 2000));
 
-    if(name && password){
-      return {message: 'Data Submitted'}
-    } else{
-      return {error: 'Error in Data input'}
+    if (name && password) {
+      return { message: 'Data Submitted' }
+    } else {
+      return { error: 'Error in Data input' }
     }
-    
+
   }
-  const [data,action,pending] = useActionState(handleFormSubmit,undefined);
+  const [data, action, pending] = useActionState(handleFormSubmit, undefined);
 
   return (
     <div>
       <h1>useActionState Hook</h1>
       <form action={action}>
-        <input type="text" placeholder="Enter Your Name" name="name"/>
+        <input type="text" placeholder="Enter Your Name" name="name" />
         <br />
-        <input type="password" placeholder="Enter Your Password" name="password"/>
+        <input type="password" placeholder="Enter Your Password" name="password" />
         <br /><br />
         <button disabled={pending}>Submit Data</button>
         <br />
-        {
-          data?.error && <span style={{color:'red'}}>{data.error}</span>
-        }
-        {
-          data?.message && <span style={{color:'green'}}>{data.message}</span>
-        }
       </form>
+      {
+        data?.error && <span style={{ color: 'red' }}>{data.error}</span>
+      }
+      {
+        data?.message && <span style={{ color: 'green' }}>{data.message}</span>
+      }
     </div>
   )
 }
