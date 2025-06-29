d7131d3c5a9caa8cc75f7147fb06da93a4e227c6
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-29 10:38:35 +0530

    Updating Array in State part1-clean up and basic set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,20 @@ import { useState } from "react";
 
 function App() {
 
-  const[data,setData] = useState(
-    {
-      name: 'Sumit',
-      address:{
-        city: 'Kolkata',
-        country: 'India'
-      }
-    }
-  )
-
-  const divStyle = {
-    border: '2px solid green',
-    width: 'fit-content',
-    padding: '2px',
-    margin: '2px',
-    borderRadius: '5px'
-  }
-
-  function handleNameUpdate(val){
-    data.name=val;
-    setData({...data});
-  }
- function handleCityUpdate(city){
-    let tempAddress=data.address;
-    tempAddress.city=city;
-    console.log(data);
-    setData({...data,address:{...tempAddress}});
-  }
+  const [data,setData] =useState(
+    [
+      'Sam','Ram','Jadu','Madhu'
+    ]
+  );
 
   return (
     <div>
-      <h1>Updating Objects in State</h1>
-
-      <input type="text" placeholder="Update Name" onChange={(event)=>handleNameUpdate(event.target.value)}/>
-      <br />
-      <input type="text" placeholder="Update City" onChange={(event)=>handleCityUpdate(event.target.value)}/>
-      <div style={divStyle}>
-        <p>Name: {data.name}</p>
-        <p>Name: {data.address.city}</p>
-        <p>Name: {data.address.country}</p>
-      </div>
+      <h1>Updating Array in State</h1>
+      {
+        data.map((item,index)=>(
+          <h3 key={index}>{item}</h3>
+        ))
+      }
     </div>
   )
 }
--- src/DisplayUser.jsx
+++
@@ -1,9 +0,0 @@
-function DisplayUser({user}){
-    return(
-        <div>
-            <h3>{user}</h3>
-        </div>
-    )
-}
-
-export default DisplayUser
