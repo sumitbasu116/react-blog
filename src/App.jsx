1e942c765e766a78502c8dce239cff8b6b0995da
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-29 11:22:57 +0530

    useActionState Hook part1-basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,16 @@
-import { useState } from "react";
 
 function App() {
 
-  const [data,setData] =useState(
-    [
-      'Sam','Ram','Jadu','Madhu'
-    ]
-  );
-  const [dataDetail,setDataDetails]=useState(
-    [
-      {Name:'Sam',Age:29},
-      {Name:'Ram',Age:23},
-      {Name:'Jadu',Age:24},
-      {Name:'Madhu',Age:22}
-    ]
-  );
-  function handleLastNameUpdate(name){
-    data[data.length-1] = name;
-    setData([...data]);
-  }
-  function handleLastElementAgeUpdate(value){
-    let tempData=dataDetail[data.length-1];
-    tempData.Age=value;
-    dataDetail[data.length-1]=tempData;
-    setDataDetails([...dataDetail]);
-  }
   return (
     <div>
-      <h1>Updating Array in State</h1>
-      <input type="text" placeholder="Update Last Name" onChange={(e)=>handleLastNameUpdate(e.target.value)}/>
-      {
-        data.map((item,index)=>(
-          <h3 key={index}>{item}</h3>
-        ))
-      }
-      <hr />
-      <input type="number" placeholder="Update Last Element Age" onChange={(e)=>handleLastElementAgeUpdate(e.target.value)}/>
-      {
-        dataDetail.map(
-          (item,index)=>(
-            <h3 key={index}>{item.Name},{item.Age}</h3>
-          )
-        )
-      }
+      <h1>useActionState Hook</h1>
+      <form action="action">
+        <input type="text" placeholder="Enter Your Name" name="name"/>
+        <br />
+        <input type="password" placeholder="Enter Your Password" name="password"/>
+        <br /><br />
+        <button>Submit Data</button>
+      </form>
     </div>
   )
 }
