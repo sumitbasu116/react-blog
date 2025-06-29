3136bce78097afde242ea974fc2bf9207d1e169b
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-06-29 10:46:15 +0530

    Updating Array in State part2-Updating Array in State

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ function App() {
       'Sam','Ram','Jadu','Madhu'
     ]
   );
-
+  function handleLastNameUpdate(name){
+    data[data.length-1] = name;
+    setData([...data]);
+  }
   return (
     <div>
       <h1>Updating Array in State</h1>
+      <input type="text" placeholder="Update Last Name" onChange={(e)=>handleLastNameUpdate(e.target.value)}/>
       {
         data.map((item,index)=>(
           <h3 key={index}>{item}</h3>
