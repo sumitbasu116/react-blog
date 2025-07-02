0b272a9e218fba632ee5b49c462b4737bea0a85a
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-02 22:56:39 +0530

    useId Hook part1-basic structure to understand the use case

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,29 @@
-import { useActionState } from "react";
+
 function App() {
-
-  async function handleFormSubmit(previousData, formData) {
-
-    let name = formData.get('name');
-    let password = formData.get('password');
-
-    await new Promise(res => setTimeout(res, 2000));
-
-    if (name && password) {
-      return { message: 'Data Submitted',name,password }
-    } else {
-      return { error: 'Error in Data input' }
-    }
-
-  }
-  const [data, action, pending] = useActionState(handleFormSubmit, undefined);
 
   return (
     <div>
-      <h1>useActionState Hook</h1>
-      <form action={action}>
-        <input type="text" defaultValue={data?.name} placeholder="Enter Your Name" name="name" />
-        <br />
-        <input type="password" defaultValue={data?.password} placeholder="Enter Your Password" name="password" />
-        <br /><br />
-        <button disabled={pending}>Submit Data</button>
-        <br />
-      </form>
-      {
-        data?.error && <span style={{ color: 'red' }}>{data.error}</span>
-      }
-      {
-        data?.message && <span style={{ color: 'green' }}>{data.message}</span>
-      }
-      {
-        data?.name && <h4>Name: {data?.name}</h4>
-      }
-      {
-        data?.password && <h4>Password: {data?.password}</h4>
-      }
+      <h1>useId Hook</h1>
+      <UserForm/>
+      <hr />
+      <UserForm/>
     </div>
   )
 }
 export default App;
+
+
+function UserForm() {
+
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="name">Enter Name:</label>
+        <input type="text" placeholder="enter your name" id="name"/>
+        <br /><br />
+        <label htmlFor="age">Enter Age:</label>
+        <input type="number" placeholder="enter your age" id="age"/>
+      </form>
+    </div>
+  )
+}
