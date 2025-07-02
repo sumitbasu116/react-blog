b8b8fb183eda06864d10883513e53bc245686f45
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-02 23:22:44 +0530

    fragment part2-using shortcut of fragment tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Fragment } from "react";
 
 function App() {
   return (
-    <Fragment>
+    <>
       <Data/>
       <hr />
       <Data/>
-    </Fragment>
+    </>
   )
 }
 export default App;
@@ -14,9 +14,9 @@ export default App;
 
 function Data() {
   return (
-    <Fragment>
+    <>
       <h1>using fragment</h1>
       <h1>using fragment</h1>
-    </Fragment>
+    </>
   )
 }
