541dff28a52315ffec0a39fb66469d0f3a135b0a
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 21:50:38 +0530

    Custom Hooks part0-clean up and basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useState } from "react";
-import College from "./College";
-import { SubjectContext } from "./ContextData";
 
 function App() {
-  const [subject,setSubject] = useState("");
   return (
-    <div style={{ backgroundColor: "green", padding: 10 }}>
-      <SubjectContext.Provider value={subject}>
-        <h1>Context API</h1>
-        <select value={subject} onChange={(event)=>setSubject(event.target.value)}>
-          <option value="">Select Subject</option>
-          <option value="Math">Math</option>
-          <option value="English">English</option>
-          <option value="Physics">Physics</option>
-          <option value="Biology">Biology</option>
-        </select>
-        <College />
-        <button onClick={()=>setSubject('')}>Clear Subject</button>
-      </SubjectContext.Provider>
-    </div>
+    <>
+    <button>Toggle Value</button>
+    <button>Show Value</button>
+    <button>Hide Value</button>
+      <h1>Custom Hooks</h1>
+    </>
   )
 }
 export default App;
