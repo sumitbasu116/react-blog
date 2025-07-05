ac2f12005b8e18883559197628d7113599a030fb
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 21:31:01 +0530

    Context API part2-actual contxt API implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import College from "./College";
+import { SubjectContext } from "./ContextData";
 
 function App() {
+  const [subject,setSubject] = useState("");
   return (
-    <div style={{backgroundColor:"green",padding:10}}>
-      <h1>Context API</h1>
-      <College/>
+    <div style={{ backgroundColor: "green", padding: 10 }}>
+      <SubjectContext.Provider value={subject}>
+        <h1>Context API</h1>
+        <select  onChange={(event)=>setSubject(event.target.value)}>
+          <option value="">Select Subject</option>
+          <option value="Math">Math</option>
+          <option value="English">English</option>
+          <option value="Physics">Physics</option>
+          <option value="Biology">Biology</option>
+        </select>
+        <College />
+      </SubjectContext.Provider>
     </div>
   )
 }
