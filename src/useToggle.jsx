a9e9683ce3b653debfaf3a722b67f125c1d64f2a
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 21:58:01 +0530

    Custom Hooks part1-creating useToggle custom component

---
+++ src/useToggle.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+const useToggle = (defaultVal)=>{
+    
+    const [value,setValue] = useState(defaultVal);
+
+    function toggleValue(val){
+        if( typeof val != 'boolean'){
+            setValue(!value);
+        } else{
+            setValue(val);
+        }
+    }
+    return (value,toggleValue)
+}
+export default useToggle;
