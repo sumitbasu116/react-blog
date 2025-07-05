fb569254b495e12ef05bf3b9b24bcef0fb7ab0e7
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 12:21:56 +0530

    Context API part0-cleaning the code before we start the Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { Fragment } from "react";
 
 function App() {
   return (
     <>
-      <Data/>
-      <hr />
-      <Data/>
+      
     </>
   )
 }
 export default App;
-
-
-function Data() {
-  return (
-    <>
-      <span>Hello</span><b>User</b>
-    </>
-  )
-}
