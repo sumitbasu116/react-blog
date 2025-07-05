795c239ce213c3d7ce7330dafc140a0c186479fc
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 21:37:35 +0530

    Context API part3-clear the subject

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div style={{ backgroundColor: "green", padding: 10 }}>
       <SubjectContext.Provider value={subject}>
         <h1>Context API</h1>
-        <select  onChange={(event)=>setSubject(event.target.value)}>
+        <select value={subject} onChange={(event)=>setSubject(event.target.value)}>
           <option value="">Select Subject</option>
           <option value="Math">Math</option>
           <option value="English">English</option>
@@ -16,6 +16,7 @@ function App() {
           <option value="Biology">Biology</option>
         </select>
         <College />
+        <button onClick={()=>setSubject('')}>Clear Subject</button>
       </SubjectContext.Provider>
     </div>
   )
