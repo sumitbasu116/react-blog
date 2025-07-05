c68dea9baaab60259e66bb1d730997f8ef84151e
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-05 21:21:19 +0530

    Context API part1-basic struture and printing the subject context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import College from "./College";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div style={{backgroundColor:"green",padding:10}}>
+      <h1>Context API</h1>
+      <College/>
+    </div>
   )
 }
 export default App;
---
+++ src/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import Student from "./Student";
+
+function ClassComponent() {
+  return (
+    <div style={{backgroundColor:"orange",padding:10}}>
+      <h1>Class Component</h1>
+      <Student/>
+    </div>
+  )
+}
+
+export default ClassComponent;
